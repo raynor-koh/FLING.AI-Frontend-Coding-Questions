e0f652251a6b65bb84675505e12f7af71299969d
raynor-koh <raynorkoh01@outlook.com>
2025-03-04 17:39:40 +0700

    Add requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(contact_form_blueprint, url_prefix="/api")
 
 if __name__ == "__main__":
+    # run with flask run --debug instead of flask run
     app.run(debug=True)
